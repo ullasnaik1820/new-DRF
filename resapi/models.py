eeae4f5357c91715278e6742b88a304007177e4e
ullasnaik1820 <151712763+ullasnaik1820@users.noreply.github.com>
2024-09-11 15:49:00 +0530

    second commit

--- resapi/models.py
+++ resapi/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Student1(models.Model):
+    name=models.CharField(max_length=100)
+    city=models.CharField(max_length=100)
+    roll=models.ImageField()
